e7c08881ef0d741c9655d565c11eb799deb7b99b
wb-zyf347708 <wb-zyf347708@alibaba-inc.com>
2019-04-04 16:08:07 +0800

    +

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,6 +7,16 @@ import React, { Component } from 'react'
 import './product.less'
 
 class Product extends Component {
+    componentDidMount () {
+        // function* a(){
+        //     yield 1
+        //     yield 2
+        //     yield 3
+        // }
+        // console.log(a().next())
+        // console.log(a().next())
+        // console.log(a().next())
+    }
 
     render() {
         return (
