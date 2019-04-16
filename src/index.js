cdffc98391df26e4a7bbe7510373655a5c95ba48
muyu <wb-zyf347708@alibaba-inc.com>
2019-04-16 12:33:38 +0800

    +

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Product from './components/Product';
-import About from './components/About';
 
 ReactDOM.render(
     <Product />,
